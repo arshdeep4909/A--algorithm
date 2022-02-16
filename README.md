d84b6f2a7015d423d4c212357c6d73c0368d1ff6
arshdeep singh <arshdeep4909@gmail.com>
2022-02-15 20:20:01 -0500

    1.added openSet, closedSet. 2. created show function for open and closed Set

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
+# Notes on p5 library
+
+we have two function
+function setup(){
+
+}
+
+function draw(){
+
+}
+now draw is a function that will keep looping. this can be used to create animations
+
+createCanvas- creates a canvas and appends it to out HTML document.
+
+------- In p5 we cannot assign variables using p5functions and variables before setup-----
+
+the reason for this is the order in which things are run, the script is run before the
+p5 library is loaded, so we have two options to solve this problem
+
+1.  let n;
+    function setup() {
+    createCanvas(100, 100);
+    n = random(100);
+    }
+
+if we declare variables before set up then we need to assign them values inside set up
+
+OR
+
+2.
+
+new p5();
+
+var boop = random(100);
+
+function setup() {
+createCanvas(100, 100);
+}
+
+function draw() {
+background(255, 0, boop);
+}
+
+here we did not assign values inside the function but we have called the p5 library
+right before we defined the variable, this is called on demand global mode.
+so, we don't get an error
+
+if we use this then we get a warning that p5 is imported twice
+
 # A--algorithm
 
 Install the p5.js library, can reference to the file online using link
@@ -31,3 +80,6 @@ open set - possibly an array that contains the nodes that still needs to be eval
 openSet and closedSet can either be a map, array or list
 here we are using them as an array but we can use whatever data structure that we wish to use
 algorithm is finished when open set is empty OR we have reached the destination
+
+-- openSet has only one node: which is the starting node
+-- closedSet starts as empty
--- index.js
+++ index.js
@@ -2,23 +2,42 @@ let grid = new Array();
 let cols = 5;
 let rows = 5;
 
+// defining openSet and closedSet as Arrays  in global variables
 // creating an object for each spot
+let openSet = [];
+let closedSet = [];
+// following p5 principles defining variables outside and then
+// assigning values inside the setup() OR draw()
+let start;
+let end;
+//defining for width and height of the rectangle
+let w, h;
+
 function Spot(i, j) {
   this.i = i;
   this.j = j;
   this.f = 0;
   this.g = 0;
   this.h = 0;
+
   // every spot has a location (i, j)
   // and they also have a f, g and h value
+
+  // this creates a rectangle for each cell in the grid
+  // we call this method in draw function()
+  this.show = function (col) {
+    fill(col);
+    rect(this.i * w, this.j * h, w, h);
+  };
 }
 
-new p5();
 function setup() {
   createCanvas(400, 400);
 
   console.log("A*");
-
+  // widht and height is the one define in our createCanvas property
+  w = width / cols;
+  h = height / rows;
   // creating a 2D array
   for (let i = 0; i < cols; i++) {
     grid[i] = new Array(rows);
@@ -33,7 +52,45 @@ function setup() {
     }
   }
 
+  // defining the start and the end node
+  //   top left to bottom right
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+  //adding our starting point to the openSet
+  openSet.push(start);
+
   console.log(grid);
 }
 
-setup();
+// the algorithm itself is a loop that keep running
+// utill we either have an empty openset or we reach
+//  the destination hence draw function keeps running in a loop
+
+// draw function keeps looping as we are using p5
+function draw() {
+  if (openSet.length > 0) {
+    //keep checking
+  } else {
+    //no solution
+  }
+
+  background(0);
+
+  // defninig function so each spot is shown when it is tested
+  // now each gir[i][j] is a spot which a constructor function
+  // and it has a property called show so we can use it as we did here with
+  // dot notation
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show(color(255));
+    }
+  }
+
+  for (let i = 0; i < closedSet.length; i++) {
+    closedSet[i].show(color(255, 0, 0));
+  }
+
+  for (let i = 0; i < openSet.length; i++) {
+    openSet[i].show(color(0, 255, 0));
+  }
+}
